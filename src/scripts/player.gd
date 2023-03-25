2df98433edbabc380c457a258d0062b12ae7b1da
tylercomisky <tdcomisky@gmail.com>
2023-03-25 04:21:15 -0400

    added in artwork

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -26,11 +26,11 @@ func _physics_process(delta):
 		
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		# See the note below about boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
+	elif velocity.y < 0:
 		$AnimatedSprite2D.animation = "up"
+	elif velocity.y > 0:
+		$AnimatedSprite2D.animation = "down"
 		
 	move_and_collide(velocity * delta)
 
