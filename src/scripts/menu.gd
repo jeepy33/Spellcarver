60cd1fb3b508a8ed73c55c56f4e0dd033b73b800
tylercomisky <tdcomisky@gmail.com>
2023-03-24 21:42:51 -0400

    added shop scene

--- src/scripts/menu.gd
+++ src/scripts/menu.gd
@@ -7,9 +7,9 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+#func _process(delta):
+	#pass
 
 
 func _on_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://src/scenes/shop.tscn")
