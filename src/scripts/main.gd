3e85f706fa8659371b78f73dd70ca32091887a04
tylercomisky <tdcomisky@gmail.com>
2023-03-25 16:23:57 -0400

    fixed merge

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -3,8 +3,7 @@ extends Node
 var starting_quadrant = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var rng = RandomNumberGenerator.new()
-	Global.master_quadrant = rng.randi_range(0,3)
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
